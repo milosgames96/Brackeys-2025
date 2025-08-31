1918da9b1ea3a705179d3f1bd7baf286d012aa87
gomnarija <lazarkarasicevic@protonmail.com>
2025-08-31 11:51:08 +0200

    gamemanager fix

--- Assets/Scripts/Main/GameManager.cs
+++ Assets/Scripts/Main/GameManager.cs
@@ -46,7 +46,7 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && !exitButton.IsActive())
         {
             TogglePauseMenu();
         }
